74ef217e124a7458844314983ea74d1f1062cc2e
RahatRezwan <kazi.rahat.rezwan889@gmail.com>
2023-10-02 17:37:58 +0600

    create message service instances for messages controller

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,20 +1,38 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, NotFoundException } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+MessagesService
 
 
 @Controller('messages')
 export class MessagesController {
+
+    messagesService: MessagesService
+
+    constructor() {
+        // don't do this in real app 
+        //use dependency injection
+        this.messagesService = new MessagesService()
+    }
+
     @Get()
     listMessages() {
+        return this.messagesService.findAll();
     }
 
     @Post()
     createMessage(@Body() body: CreateMessageDto) {
-        console.log(body);
+        return this.messagesService.create(body.content)
     }
 
     @Get('/:id')
-    getMessage(@Param('id') id: string) {
-        console.log({ id });
+    async getMessage(@Param('id') id: string) {
+        const message= await this.messagesService.findOne(id)
+
+        if(!message) {
+            throw new NotFoundException('Message Not Found')
+        }
+
+        return message
     }
 }
