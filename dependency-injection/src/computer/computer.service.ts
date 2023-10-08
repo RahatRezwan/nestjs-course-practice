0174c3c691d00813b735140e79b5d2bb4cd233ba
RahatRezwan <kazi.rahat.rezwan889@gmail.com>
2023-10-08 14:23:36 +0600

    add dependency-injection module

---
+++ dependency-injection/src/computer/computer.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { CpuService } from 'src/cpu/cpu.service';
+import { DiskService } from 'src/disk/disk.service';
+
+@Injectable()
+export class ComputerService {
+    constructor(private cpuService: CpuService, private diskService: DiskService) { }
+}
