c2ae550f9f967740d603ba0e90104c1adf1c438e
RahatRezwan <kazi.rahat.rezwan889@gmail.com>
2023-10-17 18:01:17 +0600

    update user and reports modules and create db

--- 3_car-value/src/users/users.service.ts
+++ 3_car-value/src/users/users.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
-
+import { Get, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+
+  @Get()
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  @Get()
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  update() {}
+
+  remove() {}
+}
